068d464833a2fe554ab54bf219c9977caef8ee6f
Javier Zepeda Avello <javier@MacBook-Pro-de-Javier.local>
2020-08-27 13:41:08 -0400

    First Upload

---
+++ a_maker.py
@@ -0,0 +1,45 @@
+from plistlib import *
+from funciones import *
+import valores as v
+from about import *
+
+print(f'''============================================
+Creador de Horario | Versión {version}
+Creado por: Javier Zepeda Avello – javier@uc.cl
+
+''')
+
+#Lo que se debe actualizar es: ColorSettings y WeekEvents
+archivo = dict(
+    Settings = dict(
+        ColorSettings = dict(),
+        NumberOfWeeks = 1,
+        SelectedWeek = 0,
+        WeekendDaysAreActive = True
+        ),
+    TaskCategories = [],
+    TaskEvents = [],
+    WeekEvents = []
+    )
+
+nuevo_ramo(archivo)
+cond = True
+while cond:
+    new = input("¿Desea añadir un nuevo curso? (S/n): ")
+    if sino(new):
+        nuevo_ramo(archivo)
+    else:
+        cond = False
+
+
+
+#fileName = "Timetable.timetable"
+
+#with open(fileName, 'rb') as fp:
+#    pl = load(fp)
+#hor = pl["WeekEvents"]
+
+fileName = "archivoGenerado.timetable"
+
+with open(fileName, 'wb+') as fp:
+    dump(archivo, fp)
---
+++ about.py
@@ -0,0 +1,3 @@
+version = 1.0
+author = "Javier Zepeda Avello"
+mail = "javier@uc.cl"
